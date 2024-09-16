3b4450b0dd6e53a5e706f06fb9bef178f4fa9d14
igurvir <gurvirsingh0504@gmail.com>
2024-09-15 23:02:47 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+client.once('ready', () => {
+    console.log('FeelGoodBot is online!');
+});
+
+client.login(process.env.DISCORD_TOKEN);
